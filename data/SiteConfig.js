c56204c47fa006067bf2178cc277ab96f284639b
Sam Killermann <sK@hues.xyz>
2020-10-28 17:22:46 -0500

    add favicon :)

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -2,14 +2,13 @@ const config = {
   siteTitle: "Virtual Facilitator Cards", // Site title.
   siteTitleShort: "virtual.facilitator.cards", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: "Virtual Facilitator Cards: A Facilitation Guide for Zoom and Beyond", // Alternative site title for SEO.
-  siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://virtual.facilitator.cards", // Domain of website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. 
   siteDescription: "Activities for easy, creative virtual facilitation on Zoom", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
-  siteFBAppID: "1XXXXXXXXXXX9", // FB Application ID for using app insights
-  googleAnalyticsID: "MM-XXXXXXXX-1", // GA tracking ID.
-  postDefaultCategoryID: "Tech", // Default category for posts.
+  siteFBAppID: "3406611529419983", // FB Application ID for using app insights
+  googleAnalyticsID: "UA-135058634-1", // GA tracking ID.
+  postDefaultCategoryID: "Updates", // Default category for posts.
   dateFormat: "MM/DD/YYYY", // Date format for display.
   copyright: "Uncopyright © 2020", // Copyright string for the footer of the website and RSS feed.
   themeColor: "#3DDBD4", // Used for setting manifest and progress theme colors.
--- gatsby-config.js
+++ gatsby-config.js
@@ -118,18 +118,7 @@ module.exports = {
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: "minimal-ui",
-        icons: [
-          {
-            src: "/logos/logo-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/logos/logo-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
+        icon: `src/img/icon.png`,
       }
     },
     "gatsby-plugin-offline"
